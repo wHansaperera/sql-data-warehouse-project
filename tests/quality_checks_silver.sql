d342499295e0ef5c0fd66dac3ee5e76748c1938a
Hansa perera <112180549+wHansaperera@users.noreply.github.com>
2026-07-13 01:14:09 +0530

    Enhance quality_checks_silver.sql with script purpose
    Added documentation for quality checks on silver layer data.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,3 +1,29 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'silver.crm_cust_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+
 USE DataWarehouse;
 GO
 
