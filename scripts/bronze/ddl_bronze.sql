1c7ec0d468b6884e9f27f5993fbf1b2a291ace91
Hansa perera <112180549+wHansaperera@users.noreply.github.com>
2026-07-13 13:48:13 +0530

    Update DDL for bronze tables with new structure

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,6 +9,9 @@ Script Purpose:
 ===============================================================================
 */
 
+USE DataWarehouse;
+GO
+
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
 GO
@@ -24,37 +27,40 @@ CREATE TABLE bronze.crm_cust_info (
 );
 GO
 
+
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
 GO
 
 CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+    prd_id        INT,
+    prd_key       NVARCHAR(50),
+    prd_nm        NVARCHAR(50),
+    prd_cost      INT,
+    prd_line      NVARCHAR(50),
+    prd_start_dt  DATETIME,
+    prd_end_dt    DATETIME
 );
 GO
+
 
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
 GO
 
 CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+    sls_ord_num   NVARCHAR(50),
+    sls_prd_key   NVARCHAR(50),
+    sls_cust_id   INT,
+    sls_order_dt  INT,
+    sls_ship_dt   INT,
+    sls_due_dt    INT,
+    sls_sales     INT,
+    sls_quantity  INT,
+    sls_price     INT
 );
 GO
+
 
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
@@ -66,6 +72,7 @@ CREATE TABLE bronze.erp_loc_a101 (
 );
 GO
 
+
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
 GO
@@ -76,6 +83,7 @@ CREATE TABLE bronze.erp_cust_az12 (
     gen    NVARCHAR(50)
 );
 GO
+
 
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
